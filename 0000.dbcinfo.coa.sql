3553d1f9fdba1049c8874b1669b53767b7cf7629
tdcoa <stephen@familyhilton.com>
2020-02-14 15:43:00 -0800

    revamped demo sql slightly (dbcinfo)

--- 0000.dbcinfo.coa.sql
+++ 0000.dbcinfo.coa.sql
@@ -1,11 +1,11 @@
 /*{{save:{siteid}.dbcinfo.csv}}*/
 /*{{load:adlste_coa.stg_dat_dbcinfo}}*/
 /*{{call:adlste_coa.sp_dat_dbcinfo()}}*/
-
-Select '{account}' as Account_Name, '{siteid}' as Site_ID, d.*
-,'Initial Load' as Record_Status
-,0 as Process_ID
-,Current_Timestamp(0) as Process_TS
+Select
+ '{account}' as Account_Name
+,'{siteid}' as Site_ID
+,d.InfoKey
+,d.InfoData
 from dbc.dbcinfo as d
 ;
 
