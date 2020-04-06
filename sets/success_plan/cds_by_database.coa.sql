6e9b694cef6a82c58f7990c0c03eaf6c70ce4fd2
tdcoa <stephen@familyhilton.com>
2020-04-06 09:33:58 -0700

    -- bug fix for cds_by_database

--- sets/success_plan/cds_by_database.coa.sql
+++ sets/success_plan/cds_by_database.coa.sql
@@ -24,13 +24,13 @@ Select
 FROM (
   SELECT
      case when d.DatabaseName is null
-          then '--Totals--'
+          then '+=+= Totals =+=+'
           else d.DatabaseName end as DBName
     ,case when d.DatabaseName is null
-          then '--Entire Teradata System (minus 20% spool)--'
+          then '+=+= Entire Teradata System (minus 20% spool) =+=+'
           else Max(d.CommentString) end as CommentString
     ,cast(sum(MaxPerm)/1e9 as decimal(16,1))
-     * case when DBName='--Totals--'  then 0.8 else 1 end as MaxPermGB
+     * case when DBName='+=+= Totals =+=+'  then 0.8 else 1 end as MaxPermGB
     ,ZeroIfNull(cast(NullifZero(sum(CurrentPerm))
       /1e9 as decimal(16,1))) as CurrentPermGB
     ,ZeroIfNull(cast(CurrentPermGB as decimal(16,4))
@@ -64,13 +64,13 @@ FROM (
        ,sum( case when k.Table_Bucket in('Function') then 1 else 0 end) as FunctionCount
        ,sum( case when k.Table_Bucket in('Other','Journal') then 1
                   when k.Table_Bucket is null then 1 else 0 end) as OtherCount
-          FROM dbc.Databases as d
+      FROM dbc.Databases as d
       LEFT OUTER JOIN dbc.Tables as t
         ON t.DatabaseName = d.DatabaseName
       LEFT OUTER JOIN "dim_tablekind.csv" as k
         ON t.TableKind = k.TableKind
       Group  By 1,2
  ) d
-      ON s.DatabaseName = d.DatabaseName
+    ON s.DatabaseName = d.DatabaseName
     GROUP BY rollup (d.Databasename)
  ) d
