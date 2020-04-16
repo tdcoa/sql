4e6650844c359dc0679a01bbd004150c76bbd27b
tdcoa <stephen@familyhilton.com>
2020-04-15 23:28:02 -0700

    dim_feature to coat_dim_feature

--- sets/csm_mbo/create_dim_feature.sql
+++ sets/csm_mbo/create_dim_feature.sql
@@ -1,5 +1,10 @@
-Create table adlste_coa.dim_feature as
+Create table adlste_coa.coat_dim_feature as
 (Select * from "dim_feature.csv"
 ) with no data
 primary index (account, bitpos  )
+;
+
+replace view adlste_coa.coa_dim_feature as
+locking row for access
+select * from adlste_coa.coat_dim_feature
 ;
--- sets/csm_mbo/upload_dim_feature.coa.sql
+++ sets/csm_mbo/upload_dim_feature.coa.sql
@@ -5,12 +5,12 @@
 /* only needed if table is missing:
  {{file:create_dim_feature.sql}}*/
 
-delete from adlste_coa.dim_feature
+delete from adlste_coa.coa_dim_feature
   where (Account_Name, DBS_Version, BitPOS) in
         (select Account_Name, DBS_Version, BitPOS
          from "dim_feature.csv")
 ;
 
-insert into adlste_coa.dim_feature
+insert into adlste_coa.coa_dim_feature
 Select * from "dim_feature.csv"
 ;
