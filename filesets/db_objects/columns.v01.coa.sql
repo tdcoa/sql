86809ddcc059dde9b281a905a2918782a1a062ab
tdcoa <stephen@familyhilton.com>
2020-06-04 10:58:27 -0700

    added db_objects fileset

---
+++ filesets/db_objects/columns.v01.coa.sql
@@ -0,0 +1,140 @@
+
+/*{{save:column_type.csv}}*/
+ SELECT
+ '{siteid}' as Site_ID
+ ,CASE ColumnType
+    WHEN 'BF' THEN 'BYTE('            || TRIM(ColumnLength (FORMAT '-(9)9')) || ')'
+    WHEN 'BV' THEN 'VARBYTE('         || TRIM(ColumnLength (FORMAT 'Z(9)9')) || ')'
+    WHEN 'CF' THEN 'CHAR('            || TRIM(ColumnLength (FORMAT 'Z(9)9')) || ')'
+    WHEN 'CV' THEN 'VARCHAR('         || TRIM(ColumnLength (FORMAT 'Z(9)9')) || ')'
+    WHEN 'D ' THEN 'DECIMAL('         || TRIM(DecimalTotalDigits (FORMAT '-(9)9')) || ','
+                                      || TRIM(DecimalFractionalDigits (FORMAT '-(9)9')) || ')'
+    WHEN 'DA' THEN 'DATE'
+    WHEN 'F ' THEN 'FLOAT'
+    WHEN 'I1' THEN 'BYTEINT'
+    WHEN 'I2' THEN 'SMALLINT'
+    WHEN 'I8' THEN 'BIGINT'
+    WHEN 'I ' THEN 'INTEGER'
+    WHEN 'AT' THEN 'TIME('            || TRIM(DecimalFractionalDigits (FORMAT '-(9)9')) || ')'
+    WHEN 'TS' THEN 'TIMESTAMP('       || TRIM(DecimalFractionalDigits (FORMAT '-(9)9')) || ')'
+    WHEN 'TZ' THEN 'TIME('            || TRIM(DecimalFractionalDigits (FORMAT '-(9)9')) || ')' || ' WITH TIME ZONE'
+    WHEN 'SZ' THEN 'TIMESTAMP('       || TRIM(DecimalFractionalDigits (FORMAT '-(9)9')) || ')' || ' WITH TIME ZONE'
+    WHEN 'YR' THEN 'INTERVAL YEAR('   || TRIM(DecimalTotalDigits (FORMAT '-(9)9')) || ')'
+    WHEN 'YM' THEN 'INTERVAL YEAR('   || TRIM(DecimalTotalDigits (FORMAT '-(9)9')) || ')'      || ' TO MONTH'
+    WHEN 'MO' THEN 'INTERVAL MONTH('  || TRIM(DecimalTotalDigits (FORMAT '-(9)9')) || ')'
+    WHEN 'DY' THEN 'INTERVAL DAY('    || TRIM(DecimalTotalDigits (FORMAT '-(9)9')) || ')'
+    WHEN 'DH' THEN 'INTERVAL DAY('    || TRIM(DecimalTotalDigits (FORMAT '-(9)9')) || ')'      || ' TO HOUR'
+    WHEN 'DM' THEN 'INTERVAL DAY('    || TRIM(DecimalTotalDigits (FORMAT '-(9)9')) || ')'      || ' TO MINUTE'
+    WHEN 'DS' THEN 'INTERVAL DAY('    || TRIM(DecimalTotalDigits (FORMAT '-(9)9')) || ')'      || ' TO SECOND('
+                                      || TRIM(DecimalFractionalDigits (FORMAT '-(9)9')) || ')'
+    WHEN 'HR' THEN 'INTERVAL HOUR('   || TRIM(DecimalTotalDigits (FORMAT '-(9)9')) || ')'
+    WHEN 'HM' THEN 'INTERVAL HOUR('   || TRIM(DecimalTotalDigits (FORMAT '-(9)9')) || ')'      || ' TO MINUTE'
+    WHEN 'HS' THEN 'INTERVAL HOUR('   || TRIM(DecimalTotalDigits (FORMAT '-(9)9')) || ')'      || ' TO SECOND('
+                                      || TRIM(DecimalFractionalDigits (FORMAT '-(9)9')) || ')'
+    WHEN 'MI' THEN 'INTERVAL MINUTE(' || TRIM(DecimalTotalDigits (FORMAT '-(9)9')) || ')'
+    WHEN 'MS' THEN 'INTERVAL MINUTE(' || TRIM(DecimalTotalDigits (FORMAT '-(9)9')) || ')'      || ' TO SECOND('
+                                      || TRIM(DecimalFractionalDigits (FORMAT '-(9)9')) || ')'
+    WHEN 'SC' THEN 'INTERVAL SECOND(' || TRIM(DecimalTotalDigits (FORMAT '-(9)9')) || ','
+                                      || TRIM(DecimalFractionalDigits (FORMAT '-(9)9')) || ')'
+    WHEN 'BO' THEN 'BLOB('            || TRIM(ColumnLength (FORMAT 'Z(9)9')) || ')'
+    WHEN 'CO' THEN 'CLOB('            || TRIM(ColumnLength (FORMAT 'Z(9)9')) || ')'
+
+    WHEN 'PD' THEN 'PERIOD(DATE)'
+    WHEN 'PM' THEN 'PERIOD(TIMESTAMP('|| TRIM(DecimalFractionalDigits (FORMAT '-(9)9')) || ')' || ' WITH TIME ZONE)'
+    WHEN 'PS' THEN 'PERIOD(TIMESTAMP('|| TRIM(DecimalFractionalDigits (FORMAT '-(9)9')) || '))'
+    WHEN 'PT' THEN 'PERIOD(TIME('     || TRIM(DecimalFractionalDigits (FORMAT '-(9)9')) || '))'
+    WHEN 'PZ' THEN 'PERIOD(TIME('     || TRIM(DecimalFractionalDigits (FORMAT '-(9)9')) || ')' || ' WITH TIME ZONE)'
+    WHEN 'UT' THEN COALESCE(ColumnUDTName,  '<Unknown> ' || ColumnType)
+
+    WHEN '++' THEN 'TD_ANYTYPE'
+    WHEN 'N'  THEN 'NUMBER('          || CASE WHEN DecimalTotalDigits = -128 THEN '*' ELSE TRIM(DecimalTotalDigits (FORMAT '-(9)9')) END
+                                      || CASE WHEN DecimalFractionalDigits IN (0, -128) THEN '' ELSE ',' || TRIM(DecimalFractionalDigits (FORMAT '-(9)9')) END
+                                      || ')'
+    WHEN 'A1' THEN COALESCE('SYSUDTLIB.' || ColumnUDTName,  '<Unknown> ' || ColumnType)
+    WHEN 'AN' THEN COALESCE('SYSUDTLIB.' || ColumnUDTName,  '<Unknown> ' || ColumnType)
+
+    WHEN 'JN' THEN 'JSON('            || TRIM(ColumnLength (FORMAT 'Z(9)9')) || ')'
+    WHEN 'VA' THEN 'TD_VALIST'
+    WHEN 'XM' THEN 'XML'
+
+    ELSE '<Unknown> ' || ColumnType
+  END
+  || CASE
+        WHEN ColumnType IN ('CV', 'CF', 'CO')
+        THEN CASE CharType
+                WHEN 1 THEN ' CHARACTER SET LATIN'
+                WHEN 2 THEN ' CHARACTER SET UNICODE'
+                WHEN 3 THEN ' CHARACTER SET KANJISJIS'
+                WHEN 4 THEN ' CHARACTER SET GRAPHIC'
+                WHEN 5 THEN ' CHARACTER SET KANJI1'
+                ELSE ''
+             END
+         ELSE ''
+      END as Column_Type,
+
+CASE ColumnType
+    WHEN 'BF' THEN 'BYTE'
+    WHEN 'BV' THEN 'VARBYTE'
+    WHEN 'CF' THEN 'CHAR'
+    WHEN 'CV' THEN 'VARCHAR'
+    WHEN 'D ' THEN 'DECIMAL'
+    WHEN 'DA' THEN 'DATE'
+    WHEN 'F ' THEN 'FLOAT'
+    WHEN 'I1' THEN 'BYTEINT'
+    WHEN 'I2' THEN 'SMALLINT'
+    WHEN 'I8' THEN 'BIGINT'
+    WHEN 'I ' THEN 'INTEGER'
+    WHEN 'AT' THEN 'TIME'
+    WHEN 'TS' THEN 'TIMESTAMP'
+    WHEN 'TZ' THEN 'TIME'
+    WHEN 'SZ' THEN 'TIMESTAMP'
+    WHEN 'YR' THEN 'INTERVAL'
+    WHEN 'YM' THEN 'INTERVAL'
+    WHEN 'MO' THEN 'INTERVAL'
+    WHEN 'DY' THEN 'INTERVAL'
+    WHEN 'DH' THEN 'INTERVAL'
+    WHEN 'DM' THEN 'INTERVAL'
+    WHEN 'DS' THEN 'INTERVAL'
+    WHEN 'HR' THEN 'INTERVAL'
+    WHEN 'HM' THEN 'INTERVAL'
+    WHEN 'HS' THEN 'INTERVAL'
+    WHEN 'MI' THEN 'INTERVAL'
+    WHEN 'MS' THEN 'INTERVAL'
+    WHEN 'SC' THEN 'INTERVAL'
+    WHEN 'BO' THEN 'BLOB'
+    WHEN 'CO' THEN 'CLOB'
+
+    WHEN 'PD' THEN 'PERIOD'
+    WHEN 'PM' THEN 'PERIOD'
+    WHEN 'PS' THEN 'PERIOD'
+    WHEN 'PT' THEN 'PERIOD'
+    WHEN 'PZ' THEN 'PERIOD'
+    WHEN 'UT' THEN COALESCE(ColumnUDTName,  '<Unknown> ' || ColumnType)
+
+    WHEN '++' THEN 'TD_ANYTYPE'
+    WHEN 'N'  THEN 'NUMBER'
+    WHEN 'A1' THEN COALESCE('SYSUDTLIB.' || ColumnUDTName,  '<Unknown> ' || ColumnType)
+    WHEN 'AN' THEN COALESCE('SYSUDTLIB.' || ColumnUDTName,  '<Unknown> ' || ColumnType)
+
+    WHEN 'JN' THEN 'JSON('            || TRIM(ColumnLength (FORMAT 'Z(9)9')) || ')'
+    WHEN 'VA' THEN 'TD_VALIST'
+    WHEN 'XM' THEN 'XML'
+
+    ELSE '<Unknown> ' || ColumnType
+  END
+  as Column_Category
+  ,count(*) as Total_Cnt
+
+FROM dbc.ColumnsV
+WHERE ColumnType IS NOT NULL
+group by 1,2
+order by 3 desc;
+
+
+/*{{save:column_format.csv}}*/
+SELECT  '{siteid}' as Site_ID,
+CASE WHEN ColumnFormat IS NOT NULL
+THEN 'FORMATTED' ELSE 'NO DEFAULT FORMAT'
+END AS COLUMN_FORMAT,
+ count(*) as Total_Cnt
+ from DBC.COlumnsV group by 1
