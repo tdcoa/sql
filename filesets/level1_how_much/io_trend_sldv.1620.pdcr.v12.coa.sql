fa4b4b43a91ec1bafc628d18906d3fcbc59c39aa
tdcoa <stephen@familyhilton.com>
2020-06-25 21:23:56 -0700

    updated for success plan

--- filesets/level1_how_much/io_trend_sldv.1620.pdcr.v12.coa.sql
+++ filesets/level1_how_much/io_trend_sldv.1620.pdcr.v12.coa.sql
@@ -64,7 +64,7 @@ WHERE  c2.calendar_date BETWEEN a5.TheDate+1 AND a5.TheDate + 365
 
 ===== SQL ===== ===== ===== ===== =====*/
 
-/*{{save:level1_iotrend_spma.coa.csv}}*/
+/*{{save:level1_io_trend_spma.coa.csv}}*/
 /*{{load:{db_stg}.stg_dat_level1_IO_Forecast}}*/
 /*{{call:{db_coa}.sp_dat_level1_IO_Forecast('{fileset_version}')}}*/
 
