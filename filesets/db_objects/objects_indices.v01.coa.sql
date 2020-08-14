b31f09d74b4b4b40c3eedefc038c3aed42bed256
JM185160 <68963950+JM185160@users.noreply.github.com>
2020-08-14 14:23:40 -0400

    Update and rename indices.v01.coa.sql to objects_indices.v01.coa.sql

--- filesets/db_objects/objects_indices.v01.coa.sql
+++ filesets/db_objects/objects_indices.v01.coa.sql
@@ -3,16 +3,18 @@
 
 /*{{temp:dim_indexkind.csv}}*/;
 
-create volatile table index_types as 
+create volatile table index_types_by_database as 
 (
  SEL
-      Inds.DatabaseName
+      Current_Date AS LogDate
+     ,Inds.DatabaseName
      ,IK.IndexKindDesc AS IndexTypeDesc
      ,COALESCE(COUNT(*), 0) AS IndexCount
  FROM "dim_indexkind.csv" AS IK
  LEFT JOIN DBC.IndicesV Inds
    ON IK.IndexKind = Inds.IndexType 
   AND IK.UniqueFlag = Inds.UniqueFlag
-group by 1,2
+group by 2,3
 ) with data 
 no primary index on commit preserve rows;
+
