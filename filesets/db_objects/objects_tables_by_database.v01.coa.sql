d8e02bdf987557955d774cbbf9ad824a4bcec7d1
JM185160 <68963950+JM185160@users.noreply.github.com>
2020-08-14 14:25:23 -0400

    Add files via upload

---
+++ filesets/db_objects/objects_tables_by_database.v01.coa.sql
@@ -0,0 +1,8 @@
+/* dimension  for table kinds
+*//*{{temp:dim_tablekind.csv}}*/;
+
+create volatile table table_kinds_by_database as 
+(SELECT     Current_Date AS LogDate    ,DatabaseName    ,Table_Bucket    ,TableKind_Desc    ,CheckOpt AS MultisetInd                         ,COUNT(*) AS ObjectCountFROM DBC.TablesV as  tJOIN "dim_tablekind.csv" as tk  on t.TableKind = tk.TableKind
+GROUP BY 2,3,4,5
+) with data 
+no primary index on commit preserve rows;
