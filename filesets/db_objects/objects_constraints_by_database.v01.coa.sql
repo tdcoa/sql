116670ded2bf1344d767dbeaca36610b4cfb69d4
JM185160 <68963950+JM185160@users.noreply.github.com>
2020-08-21 17:12:43 -0400

    Update objects_constraints_by_database.v01.coa.sql

--- filesets/db_objects/objects_constraints_by_database.v01.coa.sql
+++ filesets/db_objects/objects_constraints_by_database.v01.coa.sql
@@ -1,4 +1,6 @@
-/* Contraint analysis */                      
+/* Contraint analysis 
+Details and counts by DB
+*/
 
 create volatile table constraint_details as 
 (
@@ -6,21 +8,43 @@ create volatile table constraint_details as
               DatabaseName,
               TableName,
               CASE WHEN IndexType IN ('U','P','Q') THEN 'Unique'
-                   WHEN IndexType IN ('K') THEN 'Primary Key'              END (VARCHAR(30)) AS ConstraintType,
+                   WHEN IndexType IN ('K') THEN 'Primary Key'
+              END (VARCHAR(30)) AS ConstraintType,
               TRIM(TRAILING ',' 
                    FROM XMLAGG(ColumnName || ','
-                   ORDER BY ColumnPosition)(VARCHAR(255))) AS Details        FROM  DBC.IndicesV        WHERE IndexType IN ('K','U','P','Q') 
-          AND UniqueFlag = 'Y'        GROUP BY  DatabaseName, TableName, IndexType, IndexNumber
-        UNION ALL
-        SELECT  ChildDB,                ChildTable,                'Foreign Key',
-                '>- ' || ParentDB || '.' || ParentTable        FROM    DBC.RI_Distinct_ChildrenV
-        UNION ALL
-        SELECT  DatabaseName,                TableName,                'Column Constraint',
-                REGEXP_SUBSTR(ColumnConstraint,'\(.*',1,1)        FROM    DBC.ColumnsV        WHERE   ColumnConstraint IS NOT NULL
-        UNION ALL
-        SELECT  DatabaseName,                TableName,                'Table Constraint',
-                REGEXP_SUBSTR(ConstraintText,'\(.*',1,1)        FROM    DBC.Table_LevelConstraintsV
-        UNION ALL
+                   ORDER BY ColumnPosition)(VARCHAR(255))) AS Details
+        FROM  DBC.IndicesV
+        WHERE IndexType IN ('K','U','P','Q') 
+          AND UniqueFlag = 'Y'
+        GROUP BY  DatabaseName, TableName, IndexType, IndexNumber
+
+        UNION ALL
+
+        SELECT  ChildDB,
+                ChildTable,
+                'Foreign Key',
+                '>- ' || ParentDB || '.' || ParentTable
+        FROM    DBC.RI_Distinct_ChildrenV
+
+        UNION ALL
+
+        SELECT  DatabaseName,
+                TableName,
+                'Column Constraint',
+                REGEXP_SUBSTR(ColumnConstraint,'\(.*',1,1)
+        FROM    DBC.ColumnsV
+        WHERE   ColumnConstraint IS NOT NULL
+
+        UNION ALL
+
+        SELECT  DatabaseName,
+                TableName,
+                'Table Constraint',
+                REGEXP_SUBSTR(ConstraintText,'\(.*',1,1)
+        FROM    DBC.Table_LevelConstraintsV
+
+        UNION ALL
+
         SELECT  COL.DatabaseName,
                 COL.TableName,
                 'Default',
@@ -32,4 +56,32 @@ create volatile table constraint_details as
         AND     TAB.TableKind = 'T'
         WHERE   COL.DefaultValue IS NOT NULL
 ) with data no primary index on commit preserve rows
-;create volatile table constraint_type_by_database as (SELECT    ctt.ConstraintType  ,COALESCE(ctc.DatabaseName, '') AS DatabaseName  ,ZEROIFNULL(ctc.ConstraintCount) AS ConstraintCountFROM(  SELECT  cast('Unique' as varchar(30)) AS ConstraintType FROM (sel 1 one) i1 UNION ALL  SELECT 'Primary Key' FROM (sel 1 one) i2 UNION ALL  SELECT 'Foreign Key' FROM (sel 1 one) i3 UNION ALL  SELECT 'Column Constraint' FROM (sel 1 one) i4 UNION ALL  SELECT 'Table Constraint' FROM (sel 1 one) i5) cttLEFT JOIN(  SELECT     DatabaseName    ,ConstraintType    ,COUNT(*) AS ConstraintCount  FROM constraint_details  GROUP BY 1,2) ctc     ON ctt.ConstraintType = ctc.ConstraintType) with data no primary index on commit preserve rows;
+;
+
+
+create volatile table constraint_type_by_database as 
+(
+SELECT 
+   ctt.ConstraintType
+  ,COALESCE(ctc.DatabaseName, '') AS DatabaseName
+  ,ZEROIFNULL(ctc.ConstraintCount) AS ConstraintCount
+FROM
+(
+  SELECT  cast('Unique' as varchar(30)) AS ConstraintType FROM (sel 1 one) i1 UNION ALL
+  SELECT 'Primary Key' FROM (sel 1 one) i2 UNION ALL
+  SELECT 'Foreign Key' FROM (sel 1 one) i3 UNION ALL
+  SELECT 'Column Constraint' FROM (sel 1 one) i4 UNION ALL
+  SELECT 'Table Constraint' FROM (sel 1 one) i5
+) ctt
+LEFT JOIN
+(
+  SELECT
+     DatabaseName
+    ,ConstraintType
+    ,COUNT(*) AS ConstraintCount
+  FROM constraint_details
+  GROUP BY 1,2
+) ctc     
+ON ctt.ConstraintType = ctc.ConstraintType
+) with data no primary index on commit preserve rows
+;
