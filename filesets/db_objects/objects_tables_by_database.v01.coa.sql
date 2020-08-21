5d012e01c19e133df840b09825d63cfb9ef32555
JM185160 <68963950+JM185160@users.noreply.github.com>
2020-08-21 17:14:17 -0400

    Update objects_tables_by_database.v01.coa.sql

--- filesets/db_objects/objects_tables_by_database.v01.coa.sql
+++ filesets/db_objects/objects_tables_by_database.v01.coa.sql
@@ -1,8 +1,29 @@
 /* dimension  for table kinds
-*//*{{temp:dim_tablekind.csv}}*/;
+   Full outer join only shows when an table kind does not exist at all, regardless of database.
+   Database is only used as a filter in outer scripts.
+*/
+
+/*{{temp:dim_tablekind.csv}}*/;
 
 create volatile table table_kinds_by_database as 
-(SELECT     Current_Date AS LogDate    ,DatabaseName    ,Table_Bucket    ,TableKind_Desc    ,CheckOpt AS MultisetInd                         ,COUNT(*) AS ObjectCountFROM DBC.TablesV as  tJOIN "dim_tablekind.csv" as tk  on t.TableKind = tk.TableKind
-GROUP BY 2,3,4,5
+(
+  SELECT
+     COALESCE(DatabaseName, '') AS DatabaseName
+    ,COALESCE(tk.Table_Bucket, 'Unknown - ' || t.TableKind) AS TableBucket
+    ,COALESCE(tk.TableKind_Desc, 'Unknown - ' || t.TableKind) AS TableKindDesc
+    ,CheckOpt AS MultisetInd                     
+    ,ZEROIFNULL(ObjectCount) AS ObjectCount
+  FROM
+  (
+   SELECT
+     DatabaseName
+    ,TableKind 
+    ,CheckOpt                     
+    ,COUNT(*) AS ObjectCount
+   FROM DBC.TablesV 
+   GROUP BY 1,2,3
+  ) t
+  FULL OUTER JOIN "dim_tablekind.csv" as tk
+  on t.TableKind = tk.TableKind
 ) with data 
-no primary index on commit preserve rows;
+no primary index on commit preserve rows;
