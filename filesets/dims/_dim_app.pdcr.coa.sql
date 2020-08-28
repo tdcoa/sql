b994c35794c1c38fa3df58a8b747a385b0ebfb97
JM185160 <68963950+JM185160@users.noreply.github.com>
2020-08-28 17:21:38 -0400

    Update _dim_app.pdcr.coa.sql

--- filesets/dims/_dim_app.pdcr.coa.sql
+++ filesets/dims/_dim_app.pdcr.coa.sql
@@ -21,7 +21,11 @@ create volatile table dim_app as
   ,coalesce(p.Pattern, o.AppID)      as Pattern
   ,coalesce(p.SiteID, 'None')        as SiteID_
   from (select distinct AppID from pdcrinfo.DBQLogTbl_Hst
-        where LogDate between {startdate} and {enddate}) as o
+        where LogDate between {startdate} and {enddate}
+        union
+        select distinct AppID from pdcrinfo.DBQLSummaryTbl_Hst
+        where LogDate between {startdate} and {enddate}
+        ) as o
   left join "dim_app.csv" as p
     on (case
         when p.Pattern_Type = 'Equal' and o.AppID = p.Pattern then 1
