0f7519eb19523c4d8f5893e8443c25f08c10fd65
tdcoa <stephen@familyhilton.com>
2020-08-31 17:26:06 -0700

    update master control file for dim_dbobjects

---
+++ filesets/dims/_dim_dbobject_all.coa.sql
@@ -0,0 +1,14 @@
+/* Start COA: all dim_object tables
+   builds volatile tables for all dim_dbobject dimensions, including
+   - dim_indexkind
+   - dim_tablekind
+   - dim_datatype
+   - dim_dbobject
+   - dim_tdinternal_databases
+*/
+
+/*{{temp:dim_indextype.csv}}*/;
+/*{{temp:dim_tablekind.csv}}*/;
+/*{{temp:dim_datatype.csv}}*/;
+/*{{temp:dim_dbobject.csv}}*/;
+/*{{temp:dim_tdinternal_databases.csv}}*/;
