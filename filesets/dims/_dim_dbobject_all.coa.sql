10ec2dd8ba35d7b266bbf8fd0ce2405924aaa36b
tdcoa <stephen@familyhilton.com>
2020-09-22 15:51:44 -0700

    long-term .csv table names

--- filesets/dims/_dim_dbobject_all.coa.sql
+++ filesets/dims/_dim_dbobject_all.coa.sql
@@ -12,3 +12,15 @@
 /*{{temp:dim_datatype.csv}}*/;
 /*{{temp:dim_dbobject.csv}}*/;
 /*{{temp:dim_tdinternal_databases.csv}}*/;
+
+create table dim_indextype as "dim_indextype.csv" with data;
+create table dim_tablekind as "dim_tablekind.csv" with data;
+create table dim_datatype as "dim_datatype.csv" with data;
+create table dim_dbobject as "dim_dbobject.csv" with data;
+create table dim_tdinternal_databases as "dim_tdinternal_databases.csv" with data;
+
+drop table "dim_indextype.csv";
+drop table "dim_tablekind.csv";
+drop table "dim_datatype.csv";
+drop table "dim_dbobject.csv";
+drop table "dim_tdinternal_databases.csv";
