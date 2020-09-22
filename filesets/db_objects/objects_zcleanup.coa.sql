f3023849bef63b19d1f649c5cbcf9f4fe01014c4
tdcoa <stephen@familyhilton.com>
2020-09-22 15:41:04 -0700

    added cleanup to end of db_objects

---
+++ filesets/db_objects/objects_zcleanup.coa.sql
@@ -0,0 +1,6 @@
+/* cleanup script to remove volatile tables */
+drop table "dim_indextype.csv" ;
+drop table "dim_tablekind.csv" ;
+drop table "dim_datatype.csv" ;
+drop table "dim_dbobject.csv" ;
+drop table "dim_tdinternal_databases.csv" ;
