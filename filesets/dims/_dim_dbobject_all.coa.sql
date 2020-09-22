471dd5e3e855376a810469b81c7e86e5428c1a6b
tdcoa <stephen@familyhilton.com>
2020-09-22 16:22:35 -0700

    reformat

--- filesets/dims/_dim_dbobject_all.coa.sql
+++ filesets/dims/_dim_dbobject_all.coa.sql
@@ -5,22 +5,31 @@
    - dim_datatype
    - dim_dbobject
    - dim_tdinternal_databases
-*/
+*/;
 
-/*{{temp:dim_indextype.csv}}*/;
-/*{{temp:dim_tablekind.csv}}*/;
-/*{{temp:dim_datatype.csv}}*/;
-/*{{temp:dim_dbobject.csv}}*/;
-/*{{temp:dim_tdinternal_databases.csv}}*/;
-
-create volatile table dim_indextype as "dim_indextype.csv" with data on commit preserve rows;
-create volatile table dim_tablekind as "dim_tablekind.csv" with data on commit preserve rows;
-create volatile table dim_datatype as "dim_datatype.csv" with data on commit preserve rows;
-create volatile table dim_dbobject as "dim_dbobject.csv" with data on commit preserve rows;
-create volatile table dim_tdinternal_databases as "dim_tdinternal_databases.csv" with data on commit preserve rows;
-
+/*{{temp:dim_indextype.csv}}*/
+create volatile table dim_indextype as "dim_indextype.csv"
+  with data on commit preserve rows;
 drop table "dim_indextype.csv";
+
+
+/*{{temp:dim_tablekind.csv}}*/
+create volatile table dim_tablekind as "dim_tablekind.csv"
+  with data on commit preserve rows;
 drop table "dim_tablekind.csv";
+
+/*{{temp:dim_datatype.csv}}*/
+create volatile table dim_datatype as "dim_datatype.csv"
+  with data on commit preserve rows;
 drop table "dim_datatype.csv";
+
+
+/*{{temp:dim_dbobject.csv}}*/
+create volatile table dim_dbobject as "dim_dbobject.csv"
+  with data on commit preserve rows;
 drop table "dim_dbobject.csv";
+
+/*{{temp:dim_tdinternal_databases.csv}}*/
+create volatile table dim_tdinternal_databases as "dim_tdinternal_databases.csv"
+  with data on commit preserve rows;
 drop table "dim_tdinternal_databases.csv";
