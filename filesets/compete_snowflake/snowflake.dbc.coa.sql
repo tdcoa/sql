debf94e27829b6bfc0cff6a72164351038214312
JM185160 <68963950+JM185160@users.noreply.github.com>
2020-10-26 17:20:44 -0400

    Add files via upload

--- filesets/compete_snowflake/snowflake.dbc.coa.sql
+++ filesets/compete_snowflake/snowflake.dbc.coa.sql
@@ -6,15 +6,13 @@ Parameters:
  - db_objects
 */
 
-
-/*{{temp:dim_dbobject.csv}}*/
 /*{{save:dat_snowflake_tablekind.csv}}*/
 SELECT
      '{siteid}' as Site_ID
-    ,CAST(CAST(SUM(ObjectCount) AS FORMAT 'ZZZ,ZZZ,ZZ9') AS VARCHAR(20)) AS JICount
+    ,CAST(CAST(SUM(CASE WHEN TableKindDesc = 'Join Index' THEN ObjectCount ELSE 0 END) AS FORMAT 'ZZZ,ZZZ,ZZ9') AS VARCHAR(20)) AS JICount
+    ,CAST(CAST(SUM(CASE WHEN TableKindDesc = 'Queue Table' THEN ObjectCount ELSE 0 END) AS FORMAT 'ZZZ,ZZZ,ZZ9') AS VARCHAR(20)) AS QueueCount
 FROM table_kinds_by_database
 WHERE DatabaseName NOT IN  (select dbname from dim_tdinternal_databases)
-  AND TableKindDesc = 'Join Index'
 ;
 
 
@@ -28,7 +26,7 @@ FROM table_kinds_by_database
 ;
 
 
-/*{{save:dat_snowflake_special_data_types.csv}}*/
+/*{{save:dat_snowflake_col_types.csv}}*/
 select
  '{siteid}' as Site_ID
 ,CAST(CAST(SUM(CASE WHEN ColumnCategory = 'INTERVAL' THEN ColumnCount ELSE 0 END) AS FORMAT 'ZZZ,ZZZ,ZZ9') AS VARCHAR(20)) AS IntervalCnt
@@ -43,6 +41,7 @@ select
                     THEN ColumnCount ELSE 0 END) AS FORMAT 'ZZZ,ZZZ,ZZ9') AS VARCHAR(20)) AS Clob16MCnt
 ,CAST(CAST(SUM(CASE WHEN ColumnCategory = 'XML' OR ColumnCategory LIKE 'JSON%' THEN ColumnCount ELSE 0 END) AS FORMAT 'ZZZ,ZZZ,ZZ9') AS VARCHAR(20)) AS XMLJSONCnt
 ,CAST(CAST(SUM(CASE WHEN ColumnCategory = 'ST_GEOMETRY' THEN ColumnCount ELSE 0 END) AS FORMAT 'ZZZ,ZZZ,ZZ9') AS VARCHAR(20)) AS GeoCnt
+,CAST(CAST(SUM(CASE WHEN IdentityColumnType in ('GA','GD') THEN ColumnCount ELSE 0 END) AS FORMAT 'ZZZ,ZZZ,ZZ9') AS VARCHAR(20)) AS IdentityCnt
 from column_types
 ;
 
@@ -50,8 +49,11 @@ from column_types
 /*{{save:dat_snowflake_indextype.csv}}*/
 select
    '{siteid}' as Site_ID
-  ,CAST(CAST(SUM(CASE WHEN IndexTypeDesc = 'Unique Primary Index' THEN IndexCount ELSE 0 END) AS FORMAT 'ZZZ,ZZZ,ZZ9') AS VARCHAR(20)) AS UPI
-  ,CAST(CAST(SUM(CASE WHEN IndexTypeDesc LIKE 'Partitioned Primary Index%' THEN IndexCount ELSE 0 END) AS FORMAT 'ZZZ,ZZZ,ZZ9') AS VARCHAR(20)) AS PPI
+  ,CAST(CAST(SUM(CASE WHEN IndexTypeBucket = 'Primary Index' THEN IndexCount ELSE 0 END) AS FORMAT 'ZZZ,ZZZ,ZZ9') AS VARCHAR(20)) AS UPINUPI
+  ,CAST(CAST(SUM(CASE WHEN IndexTypeBucket = 'Partition' THEN IndexCount ELSE 0 END) AS FORMAT 'ZZZ,ZZZ,ZZ9') AS VARCHAR(20)) AS PPI
+/* Unique constraints are included as SI's - in temporal tables they are JI's.
+   PK not included as SI's - they are listed under 'constraints' */
+  ,CAST(CAST(SUM(CASE WHEN IndexTypeBucket = 'Secondary Index' THEN IndexCount ELSE 0 END) AS FORMAT 'ZZZ,ZZZ,ZZ9') AS VARCHAR(20)) AS SI
 from index_types_by_database
 Where DatabaseName NOT IN  (select dbname from dim_tdinternal_databases);
 
@@ -70,26 +72,25 @@ WHERE DatabaseName NOT IN  (select dbname from dim_tdinternal_databases)
 /*{{save:dat_snowflake_usage_per_type.csv}}*/
 SELECT
      '{siteid}' as Site_ID
-    ,FT.ObjectType
-    ,ObjectTypeDesc
-    ,CAST(CAST(ZEROIFNULL(Frequency_of_Use) AS FORMAT 'ZZZ,ZZZ,ZZ9') AS VARCHAR(20)) AS Frequency_of_Use
+    ,CAST(CAST(SUM(CASE WHEN FT.ObjectType = 'Tmp' THEN OT.Frequency_of_Use ELSE 0 END) AS FORMAT 'ZZZ,ZZZ,ZZ9') AS VARCHAR(20)) AS GTTUsage
+    ,CAST(CAST(SUM(CASE WHEN FT.ObjectType IN ('Idx', 'HIx') THEN OT.ObjectCount ELSE 0 END) AS FORMAT 'ZZZ,ZZZ,ZZ9') AS VARCHAR(20)) AS IndexCount
+    ,CAST(CAST(SUM(CASE WHEN FT.ObjectType IN ('Idx', 'HIx') THEN OT.Frequency_of_Use ELSE 0 END) AS FORMAT 'ZZZ,ZZZ,ZZ9') AS VARCHAR(20)) AS IndexUsage
 FROM
     dim_dbobject FT
     LEFT OUTER JOIN
     (
         SELECT
-            OT.ObjectType
-           ,SUM(CAST(OT.FreqofUse AS BIGINT)) AS Frequency_of_Use
-        FROM  DBC.QryLogV QL
-        INNER JOIN dbc.QryLogObjectsV OT
-        ON QL.QueryID = OT.QueryID
+             ObjectType
+            ,COUNT(DISTINCT ObjectDatabaseName || ObjectTableName || TRIM(ObjectNum)) AS ObjectCount
+            ,SUM(CAST(FreqofUse AS BIGINT)) AS Frequency_of_Use
+        FROM
+            pdcrinfo.DBQLObjTbl OT
         WHERE
-            CAST(QL.StartTime AS DATE) BETWEEN {startdate} AND {enddate}
+            LogDate BETWEEN {startdate} AND {enddate}
             AND OT.ObjectDatabaseName NOT IN (select dbname from dim_tdinternal_databases)
-            AND OT.ObjectType = 'Tmp'   -- Global Temp only
+            AND ObjectType IN ('Tmp', 'Idx', 'HIx')   -- Join Indexes not included, should they be?
         GROUP BY 1
     ) OT
     ON FT.ObjectType = OT.ObjectType
-WHERE FT.ObjectType = 'Tmp'          -- dim_dboject has all obj types but this resultset is only for Tmp
 ;
 
