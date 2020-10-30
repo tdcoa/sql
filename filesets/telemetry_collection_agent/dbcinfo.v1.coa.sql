2bf21a78db9a7d5f66e0f76d21fcdd2246304430
tdcoa <stephen@familyhilton.com>
2020-10-30 12:35:16 -0700

    quoted siteid

--- filesets/telemetry_collection_agent/dbcinfo.v1.coa.sql
+++ filesets/telemetry_collection_agent/dbcinfo.v1.coa.sql
@@ -7,5 +7,5 @@
 /*{{save:dbcinfo.{siteid}.csv}}*/
 /*{{load:adlste_coa_stg.stg_tca_dbcinfo}}*/
 /*{{call:adlste_coa.sp_tca_dbcinfo()}}*/
-Select {siteid} as Site_ID, a.*
+Select '{siteid}' as Site_ID, a.*
 from dbc.dbcinfo as a;
