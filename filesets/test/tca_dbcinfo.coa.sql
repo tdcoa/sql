cb293987848737e59bc0e8919b7dd2ab4210c28d
tdcoa <stephen@familyhilton.com>
2020-11-16 13:32:33 -0800

    updated SQL to match GTT

--- filesets/test/tca_dbcinfo.coa.sql
+++ filesets/test/tca_dbcinfo.coa.sql
@@ -4,9 +4,7 @@
 /*{{load:APP_TCA_TMP.stg_tca_dbcinfo}}*/
 /*{{call:APP_TCA_TMP.sp_tca_dbcinfo()}}*/
 select
-  InfoKey
+  '{siteid}' as Site_ID
+, InfoKey
 , InfoData
-, '{siteid}' as Site_ID
-, cast(CURRENT_TIMESTAMP(0) as timestamp(0) format'yyyy-mm-ddbhh:mi:ss')(char(19)) as TheTimeStamp
 from dbc.dbcinfo as a
-;
