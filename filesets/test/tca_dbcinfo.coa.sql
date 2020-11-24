8f9e486de84c1f5d136242c6ab5ba2a46c5cea12
tdcoa <stephen@familyhilton.com>
2020-11-23 21:00:22 -0800

    changed sp location to APP_TCA_TBL

--- filesets/test/tca_dbcinfo.coa.sql
+++ filesets/test/tca_dbcinfo.coa.sql
@@ -2,7 +2,7 @@
 
 /*{{save:dbcinfo.csv}}*/
 /*{{load:APP_TCA_TMP.stg_tca_dbcinfo}}*/
-/*{{call:APP_TCA_TMP.sp_tca_dbcinfo()}}*/
+/*{{call:APP_TCA_TBL.sp_tca_dbcinfo()}}*/
 select
   '{siteid}' as Site_ID
 , InfoKey
