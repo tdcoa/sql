8d32921ca480bed3a3ec03b3b37020d00a9f4f0d
tdcoa <stephen@familyhilton.com>
2020-12-21 18:04:43 -0800

    uploaded prework file (oops)

---
+++ filesets/prework/setup.16xx.v1.coa.sql
@@ -0,0 +1,19 @@
+
+
+/*{save:setup_environment.csv}*/
+/* DBCInfo on Steroids */
+Select InfoKey, InfoData from dbc.dbcinfo
+union all
+Select 'SystemType', SystemType FROM  TABLE (SYSLIB.MonitorSystemPhysicalConfig()) as CFG
+union all
+Select 'SystemName', SystemName FROM  TABLE (SYSLIB.MonitorSystemPhysicalConfig()) as CFG
+union all
+select 'Current Time', nowish from (select Current_Timestamp(0)(varchar(25)) as nowish) as n
+union all
+select 'Current User', usr from (select User(varchar(128)) as usr) as u
+union all
+select 'Spool Space (GB)', trim(cast(SpoolSpace/1e9 as INT)(varchar(64))) from dbc.users where username = USER
+order by 1;
+
+/*{save:setup_session.csv}*/
+Help Session;
