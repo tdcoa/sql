3f1e829a175cdcbee74b3854039ed4fd794638d1
tdcoa <stephen@familyhilton.com>
2021-01-12 17:36:39 -0800

    Added {dbprefix} to dbcinfo load and call

--- filesets/tca/tca_dbcinfo.coa.sql
+++ filesets/tca/tca_dbcinfo.coa.sql
@@ -1,8 +1,8 @@
 
 
 /*{{save:dbcinfo.csv}}*/
-/*{{load:APP_TCA_TMP.stg_tca_dbcinfo}}*/
-/*{{call:APP_TCA_TBL.sp_tca_dbcinfo()}}*/
+/*{{load:{dbprefix}APP_TCA_TMP.stg_tca_dbcinfo}}*/
+/*{{call:{dbprefix}APP_TCA_TBL.sp_tca_dbcinfo()}}*/
 select
   '{siteid}' as Site_ID
 , InfoKey
