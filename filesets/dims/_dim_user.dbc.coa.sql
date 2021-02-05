5fc79cdb46636c27b0909222a6406ae5e1e7898c
tdcoa <Stephen.Hilton@FamilyHilton.com>
2021-02-04 17:15:01 -0800

    removed hardcoded value in dim_user sql

--- filesets/dims/_dim_user.dbc.coa.sql
+++ filesets/dims/_dim_user.dbc.coa.sql
@@ -121,7 +121,7 @@ create volatile table dim_user as
         when p.Pattern_Type = 'RegEx'
          and character_length(regexp_substr(o.UserName, p.Pattern,1,1,'i'))>0 then 1
         else 0 end) = 1
-    and (lower(SiteID_) in('default','none') or lower('TRANSCEND02') like lower(SiteID_))
+    and (lower(SiteID_) in('default','none') or lower('{siteid}') like lower(SiteID_))
   qualify Priority_ = min(Priority_)over(partition by o.UserName)
 ) with data
 primary index (UserName)
